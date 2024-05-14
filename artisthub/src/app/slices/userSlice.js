f6c854d1fda383c835d82b32a2c10c2755b525a9
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-14 17:29:17 +0200

    feature: added userSlice and store

---
+++ artisthub/src/app/slices/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const userSlice = createSlice({
+
+    name: "user",
+    initialState: {
+        token: null,
+        decodeToken: null,
+    },
+
+    reducers: {
+
+        setToken: (state, action) => {
+            state.token = action.payload.token
+            state.decodeToken = action.payload.decodeToken
+        },
+
+        deleteToken: (state) => {
+
+            state.token = null
+            state.decodeToken = null
+        },
+    }
+
+})
+
+export const { setToken, deleteToken } = userSlice.actions
+export default userSlice.reducer
---
+++ artisthub/src/app/store.js
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+//todo esto son LIBRERIAS.
+import storage from "redux-persist/lib/storage";
+import userSlice from "./slices/userSlice";
+import persistStore from "redux-persist/es/persistStore";
+
+
+const reducers = combineReducers({
+    user: userSlice,
+
+});
+const persistConfig = {
+    key: "root",
+    storage,
+    transforms: [
+        encryptTransform({
+            secretKey: import.meta.env.VITE_SECRET_KEY,
+            onError: function (error) {
+                console.log(error)
+            },
+        }),
+    ],
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+})
+export const persistor = persistStore(store)
